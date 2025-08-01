2d80ae7cd044147a14e387d0aab1e686794501c3
Prince Yadav <princeyadav0136@gmail.com>
2025-08-01 13:46:42 +0530

    debounce-throttling

---
+++ javascript-interview/debouncing-throttling.js
@@ -0,0 +1,4 @@
+// Debouncing & Throttling
+// Debouncing ensures a function is only called after a certain period of inactivity.
+// Throttling ensures a function is called at most once in a specified time interval.
+
